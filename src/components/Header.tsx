dbc56c671156ec63944338db70d338686e048727
shivam <shivamm0383@gmail.com>
2024-11-26 14:47:14 +0530

    cn-utils-function-for-clsx&tailwind-merge

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,10 @@
 "use client";
 import Link from "next/link";
 
-import clsx from "clsx";
 import { usePathname } from "next/navigation";
 import { motion } from "framer-motion";
 import Logo from "./logo";
+import { cn } from "@/lib/utils";
 
 const routes = [
   {
@@ -22,27 +22,30 @@ export default function Header() {
   return (
     <header className="flex items-center justify-between border-b border-white/10 h-14 px-3 sm:px-9">
       <Logo />
-      <nav className="h-full" >
+      <nav className="h-full">
         <ul className="flex  gap-x-6 h-full text-sm">
           {routes.map((route) => (
             <li
               key={route.path}
-              className={clsx("hover:text-white flex items-center relative transition", {
-                "text-white": route.path === activePathName,
-                "text-white/50": route.path !== activePathName,
-              })}
+              className={cn(
+                "hover:text-white flex items-center relative transition",
+                {
+                  "text-white": route.path === activePathName,
+                  "text-white/50": route.path !== activePathName,
+                }
+              )}
             >
               <Link href={route.path}>{route.name}</Link>
-              {
-                route.path === activePathName && (
-                  <motion.div layoutId="header-active-link" className="bg-accent h-1 w-full absolute bottom-0"></motion.div>
-                )
-              }
+              {route.path === activePathName && (
+                <motion.div
+                  layoutId="header-active-link"
+                  className="bg-accent h-1 w-full absolute bottom-0"
+                ></motion.div>
+              )}
             </li>
           ))}
         </ul>
-      </nav> 
+      </nav>
     </header>
   );
 }
-  
--- src/components/h1.tsx
+++ src/components/h1.tsx
@@ -1,7 +1,19 @@
+import { cn } from "@/lib/utils";
+
 type H1Props = {
-    children: React.ReactNode;
+  children: React.ReactNode;
+  className?: string;
 };
 
-export default function H1({ children }:H1Props) {
-  return <h1 className="text-3xl font-bold lg:text-6xl  tracking-tight ">{children}</h1>;
+export default function H1({ children, className }: H1Props) {
+  return (
+    <h1
+      className={cn(
+        "text-3xl font-bold lg:text-6xl  tracking-tight",
+        className
+      )}
+    >
+      {children}
+    </h1>
+  );
 }
