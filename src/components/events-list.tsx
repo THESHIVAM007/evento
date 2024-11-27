ef09e1bd4054c463ca2dfd2c67e481a1b72afc87
shivam <shivamm0383@gmail.com>
2024-11-27 19:15:17 +0530

    loading-skeleton-added

--- src/components/events-list.tsx
+++ src/components/events-list.tsx
@@ -1,8 +1,17 @@
 import { EventoEvent } from "@/lib/types";
 import EventCard from "./event-card";
+import { sleeper } from "@/lib/utils";
 
-type EventsListProps = { events: EventoEvent[] };
-export default function EventsList({ events }: EventsListProps) {
+type EventListProps = {
+    city: string;
+};
+
+export default async function EventsList({city}:EventListProps) {
+  await sleeper(2000);
+  const res = await fetch(
+    `https://bytegrad.com/course-assets/projects/evento/api/events?city=${city}`
+  );
+  const events: EventoEvent[] = await res.json();
   return (
     <section className="flex flex-wrap gap-10 justify-center px-[20px]">
       {events.map((event) => (
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,6 @@ import {twMerge} from "tailwind-merge";
 export function cn(...classes:ClassValue[]) {
     return twMerge(clsx(classes))
 }
+export function sleeper(ms: number) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
