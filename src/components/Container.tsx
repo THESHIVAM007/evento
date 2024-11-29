3e273525278358cd120bc902e7973193b1ac5d69
shivam <shivamm0383@gmail.com>
2024-11-29 20:28:39 +0530

    last-commit

--- src/components/Container.tsx
+++
@@ -1,8 +0,0 @@
-
-type ContainerType = {
-    children: React.ReactNode;
-};
-
-export default function Container({ children }: ContainerType) {
-  return <div className=" flex flex-col max-w-7xl min-h-screen mx-auto bg-white/[2%] ">{children}</div>;
-}
---
+++ src/components/container.tsx
@@ -0,0 +1,11 @@
+type ContainerType = {
+  children: React.ReactNode;
+};
+
+export default function Container({ children }: ContainerType) {
+  return (
+    <div className=" flex flex-col max-w-7xl min-h-screen mx-auto bg-white/[2%] ">
+      {children}
+    </div>
+  );
+}
